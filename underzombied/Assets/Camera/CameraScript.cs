14ccf13b67e9c3a72a65670f4fd237ff462c653c
Connor <lrk312@usask.ca>
2023-01-07 00:15:30 -0600

    Added tilemaps and camera control

---
+++ underzombied/Assets/Camera/CameraScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Rigidbody2D bod;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D) && transform.position.x < 11){
+            bod.velocity = new Vector2(8, 0);
+        }
+        else if (Input.GetKey(KeyCode.A) && transform.position.x > 0){
+            bod.velocity = new Vector2(-8, 0);
+        }
+        else{
+            bod.velocity = new Vector2(0, 0);
+        }
+    }
+}
