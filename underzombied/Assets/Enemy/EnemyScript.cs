3c306394fbda181db025900e6e579669068d3292
Connor <lrk312@usask.ca>
2023-01-07 16:43:28 -0600

    misc

---
+++ underzombied/Assets/Enemy/EnemyScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public BoxCollider2D bod;
+
+    public int health = 100;
+
+    private string id = System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void HitByTower(int damage)
+    {
+        health -= damage;
+        Debug.Log("I am die: " + id + " health: " + health);
+        if (health <= 0)
+            {
+            if (gameObject != null)
+                Destroy(gameObject); // instance management
+            }
+    }
+}
