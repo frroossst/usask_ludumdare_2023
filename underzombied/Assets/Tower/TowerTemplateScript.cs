21cbd61e4b388f0e09954168fafa8e912a4aba0c
Connor <lrk312@usask.ca>
2023-01-07 12:57:56 -0600

    Added a money system

--- underzombied/Assets/Tower/TowerTemplateScript.cs
+++ underzombied/Assets/Tower/TowerTemplateScript.cs
@@ -8,21 +8,26 @@ public class TowerTemplateScript : MonoBehaviour
     public Tilemap tilemap;
     public GameObject tower;
     private Vector2 mouse;
+    public GameObject money;
+    public int Cost;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cost = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
+        int s = money.GetComponent<MoneyScript>().getMoney();
+        Debug.Log(s);
         mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector2(Mathf.Round(mouse.x + 0.5f) -0.5f, Mathf.Round(mouse.y + 0.5f) - 0.5f);
 
-        if (Input.GetMouseButtonDown(0) && tilemap.HasTile(new Vector3Int(Mathf.FloorToInt(Mathf.Round(mouse.x + 0.5f) -0.5f), Mathf.FloorToInt(Mathf.Round(mouse.y + 0.5f) - 0.5f)))){
+        if (Input.GetMouseButtonDown(0) && tilemap.HasTile(new Vector3Int(Mathf.FloorToInt(Mathf.Round(mouse.x + 0.5f) -0.5f), Mathf.FloorToInt(Mathf.Round(mouse.y + 0.5f) - 0.5f))) && money.GetComponent<MoneyScript>().getMoney() >= Cost){
             Instantiate(tower, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
             tilemap.SetTile(new Vector3Int(Mathf.FloorToInt(Mathf.Round(mouse.x + 0.5f) -0.5f), Mathf.FloorToInt(Mathf.Round(mouse.y + 0.5f) - 0.5f)),null);
+            money.GetComponent<MoneyScript>().takeMoney(100);
         }
     }
 }
